a9c5fa1b9b404d80558815523eacb0e524d5f6d6
Nithin Philips <nithin@nithinphilips.com>
2009-02-03 03:51:54 +0000

    * Adding skeleton projects for the new smoz.

---
+++ src/libSmoz/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("libSmoz")]
+[assembly: AssemblyDescription("A library to access and organize the windows start menu.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nithin Philips")]
+[assembly: AssemblyProduct("Start Menu Organizer")]
+[assembly: AssemblyCopyright("(c) 2009 Nithin Philips")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e488db7-9840-49e2-be55-eef5922a0e36")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
