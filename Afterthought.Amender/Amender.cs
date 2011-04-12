92f50b2efc5a47e6385c4376d86d72aa34724cea
jamiemthomas <jamiemthomas@hotmail.com>
2011-04-12 09:59:23 -0400

    Adding copyright header/legal disclaimer to source files--fun fun

--- Afterthought.Amender/Amender.cs
+++ Afterthought.Amender/Amender.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.Build;
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (c) VC3, Inc. All rights reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//-----------------------------------------------------------------------------
+
+using Microsoft.Build;
 using Microsoft.Build.Framework;
 using System;
 using Microsoft.Build.Utilities;
--- Afterthought.UnitTest.Target/Calculator.cs
+++ Afterthought.UnitTest.Target/Calculator.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (c) VC3, Inc. All rights reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//-----------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
