f1b0a4e9f512ec00c4c37b493498684aa5aba490
Nithin Philips <nithin@nithinphilips.com>
2011-12-23 13:10:27 -0500

    Updated AssemblyInfo files.

--- src/SMOz.Tests/Properties/AssemblyInfo.cs
+++ src/SMOz.Tests/Properties/AssemblyInfo.cs
@@ -1,35 +1,11 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SMOz.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SMOz.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("A set of tests for libSMOz features")]
+[assembly: AssemblyCompany("Nithin Philips")]
+[assembly: AssemblyProduct("SMOz (Start Menu Organizer)")]
+[assembly: AssemblyCopyright("(c) 2004-2011 Nithin Philips")]
+[assembly: AssemblyTrademark("Windows is a trademark of Microsoft Corporation")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("897c1641-d3ab-4620-96be-96684336870f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/libSmoz/Properties/AssemblyInfo.cs
+++ src/libSmoz/Properties/AssemblyInfo.cs
@@ -1,39 +1,11 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using System;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("LibSmoz")]
-[assembly: AssemblyDescription("A library to access and organize the windows start menu.")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("libSmoz")]
+[assembly: AssemblyDescription("A supporting library for automated manipulation of the Windows start menu")]
 [assembly: AssemblyCompany("Nithin Philips")]
-[assembly: AssemblyProduct("Start Menu Organizer")]
-[assembly: AssemblyCopyright("(c) 2009 Nithin Philips")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("SMOz (Start Menu Organizer)")]
+[assembly: AssemblyCopyright("(c) 2004-2011 Nithin Philips")]
+[assembly: AssemblyTrademark("Windows is a trademark of Microsoft Corporation")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-[assembly: CLSCompliant(true)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e488db7-9840-49e2-be55-eef5922a0e36")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
