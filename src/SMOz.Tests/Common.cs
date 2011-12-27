53662e8066ecb3c4eaf7809dff024ec333b7412e
Nithin Philips <nithin@nithinphilips.com>
2011-12-27 04:01:16 -0500

    Adding the doc submodule.

---
+++ src/SMOz.Tests/Common.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SMOz.Tests
+{
+    static class Common
+    {
+        public static string GetTestDataFullPath(string fileName)
+        {
+            String codebase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            codebase = codebase.Replace(@"file:///", "");
+ 
+            String strPath = System.IO.Path.GetDirectoryName(codebase);
+#if RELEASE
+            return Path.Combine(strPath, "..", "..", "test-data", fileName);
+#else
+            return Path.Combine(strPath, fileName);
+#endif
+        }
+    }
+}
