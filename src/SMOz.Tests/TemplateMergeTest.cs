e62f2c4bfe1975bf14e53d75028298a8003da01e
Nithin Philips <nithin@nithinphilips.com>
2012-01-02 15:04:39 -0500

    TemplateMergeTest now runs to success.

--- src/SMOz.Tests/TemplateMergeTest.cs
+++ src/SMOz.Tests/TemplateMergeTest.cs
@@ -44,6 +44,49 @@ Item3B
             t1.Merge(t2);
 
             Assert.AreEqual(3, t1.Count);
+            Assert.AreEqual(5, t1.First(i => i.Name == "Category1").Count);
+            Assert.AreEqual(2, t1.First(i => i.Name == "Category2").Count);
+            Assert.AreEqual(2, t1.First(i => i.Name == "Category3").Count);
+        }
+        
+        [TestMethod]
+        public void TestMergeComplex()
+        {
+            string templA = @"
+[Category1]
+@^Item1A$
+@^Item1B$
+@Item1C
+*Item1D
+
+[Category2]
+Item2A
+Item2B
+            ";
+
+            string templB = @"
+[Category1]
+Item1A
+Item1B
+Item1C
+@.*Item1D.*
+Item1E
+
+[Category3]
+Item3A
+Item3B
+            ";
+
+            // All selectors untimately become regex. Thus:
+            //      Item1A  == @^Item1A$   
+            //      *Item1D == @.*Item1D.* 
+
+            Template t1 = TemplateParser.Parse(TemplateParserTest.ReadAllLines(templA));
+            Template t2 = TemplateParser.Parse(TemplateParserTest.ReadAllLines(templB));
+
+            t1.Merge(t2);
+
+            Assert.AreEqual(3, t1.Count);
             Assert.AreEqual(6, t1.First(i => i.Name == "Category1").Count);
             Assert.AreEqual(2, t1.First(i => i.Name == "Category2").Count);
             Assert.AreEqual(2, t1.First(i => i.Name == "Category3").Count);
