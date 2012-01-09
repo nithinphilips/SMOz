752f434db33e73fca7da6d8460abe209d8f20d52
Nithin Philips <nithin@nithinphilips.com>
2012-01-09 09:45:14 -0500

    Adding the default template to distribute.

--- website/conf.py
+++ website/conf.py
@@ -194,7 +194,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = "images/Logo.pdf"
+#latex_logo = "images/Logo.pdf"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
