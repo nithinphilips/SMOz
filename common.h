a5a76ddb1f1f04054ce839ac1de8338088deda2b
Keith Brooke <wkb0001@gmail.com>
2012-01-14 15:07:40 -0500

    Initial release

---
+++ common.h
@@ -0,0 +1,12 @@
+..
+	This module contains general-use markup that can be used by any
+	other module with the statement:
+		.. include:: common.h ( like a 'c' language ?.h)
+
+.. |pagebreak| raw:: latex
+
+	\newpage
+
+
+
+.. cssclass:: docutils
